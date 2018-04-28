ed106de25eeb3ed0bacf906d7cd2925a0c08158d
Anwesh43 <anweshthecool0@gmail.com>
2018-04-28 20:31:49 +0530

    adding State class

--- linked-line.ts
+++ linked-line.ts
@@ -48,6 +48,34 @@ class Animator {
     }
 }
 
+class State {
+    scales : Array<number> = [0, 0]
+    prevScale : number = 0
+    dir : number = 0
+    j : number = 0
+
+    update(stopcb : Function) {
+        this.scales[this.j] += this.dir * 0.1
+        if (Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if (this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir = 0
+                this.prevScale = this.scales[this.j]
+                stopcb()
+            }
+        }
+    }
+
+    startUpdating(startcb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
+
 const initLinkedLineStage = () => {
     const linkedLineStage : LinkedLineStage = new LinkedLineStage()
     linkedLineStage.render()
